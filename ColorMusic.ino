04466b5aa1e60df1c734467ff3e3da106ccc9d4b
Matt French <35068805+frenchmatt@users.noreply.github.com>
2018-11-21 17:28:54 -0500

    Update ColorMusic.ino

--- ColorMusic.ino
+++ ColorMusic.ino
@@ -1,49 +1,43 @@
-#define REDPIN 5
-#define GREENPIN 6
-#define BLUEPIN 3
-#define SOUNDPIN A0
+#define REDPIN 5                                            //r
+#define GREENPIN 6                                          //g
+#define BLUEPIN 3                                           //b
+#define SOUNDPIN A0                                         //pin for reading voltage level in
 
 void setup() {
   pinMode(REDPIN, OUTPUT);
   pinMode(GREENPIN, OUTPUT);
   pinMode(BLUEPIN, OUTPUT);
   pinMode(SOUNDPIN, INPUT);
-  Serial.begin (9600);
+  Serial.begin (9600);  
 }
 
-int count = 0;
-int maxVal = 0;
-double m1 = 0, m2 = 0, m3 = 0, m4 = 0;
-int r, g, b;
-int maxReset = 5000;
-int wait = 1;
-int d = 7;
-int previous = 0;
+int maxVal = 0;                                             //used to determine each song's unique max
 
+double m1 = 0, m2 = 0, m3 = 0, m4 = 0;                      //used to split input range into 5 levels
+int d = 7;                                                  //(delta) value to make colors more reactive
+int previous = 0;                                           //level previously read in
+
+int r, g, b;                                                //red, green, and blue values to be output
+int wait = 1;                                               //delay between each loop
+
+double power = 1.5;                                         
 
 void loop() {
-  double sensorValue = pow(analogRead (SOUNDPIN), 1.5);
-  Serial.println (sensorValue, DEC);
+  double sensorValue = pow(analogRead (SOUNDPIN), power);   //reads in signal and spreads values according to power
+  Serial.println (sensorValue, DEC);                        //prints input to consol for dev use
   
-  count++;
-  //reset max to be specific per each song
-  if(count > maxReset) {
-    count = 0;
-    maxVal = 0;
-  }
+  maxVal -= 1;                                              //decrements local max to keep range updated
   
-  if(sensorValue > maxVal) {
+  if(sensorValue > maxVal) {                                //resets max and interval values
     maxVal = sensorValue;
     m1 = 0.25 * maxVal;
     m2 = 0.45 * maxVal;
     m3 = 0.55 * maxVal;
     m4 = 0.7 * maxVal; 
   }
-
-  
-
-  if(previous <= m1) {
-    if(sensorValue <= m1) {
+                                                            
+  if(previous <= m1) {                                      //compares previous range to current range
+    if(sensorValue <= m1) {                                 //and changes rgb values accordingly
       r += d;
       g -= d;
       b -= d;
@@ -177,8 +171,8 @@ void loop() {
       b += d;
     }
   }
-  
-  if (r > 255) {r = 255;}
+                                                             
+  if (r > 255) {r = 255;}                                   //resets r, g, and b values if they become too small or too large
   if (r < 0) {r = 0;}
   if (g > 255) {g = 255;}
   if (g < 0) {g = 0;}
@@ -186,9 +180,9 @@ void loop() {
   if (b < 0) {b = 0;}
   
   
-  analogWrite(REDPIN,r);
+  analogWrite(REDPIN,r);                                    //writes r, g, and b values to red, green, and blue, pins
   analogWrite(GREENPIN,g);
   analogWrite(BLUEPIN,b);
-  previous = sensorValue;
-  delay(wait);
+  previous = sensorValue;                                   //updates the previous value
+  delay(wait);                                              //delays the wait amount specified
 }
